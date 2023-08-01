9c4a0739380f6a9f1bde0af5504b762c603df1a9
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-08-01 12:27:55 -0500

    agregando el ruting principal y la pagina de favoritos

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "inicio",
+      component: () => import("../views/InicioView.vue"),
+    },
+    {
+      path: "/favoritos",
+      name: "favoritos",
+      component: () => import("../views/FavoritosView.vue"),
+    },
+  ],
 });
 
 export default router;
