564e6c274d046e1da15da55518e8eb7c109a7d9c
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-08-24 13:12:07 -0500

    definiendo el store debebidas

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import InicioView from "../views/InicioView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "inicio",
-      component: () => import("../views/InicioView.vue"),
+      component: InicioView,
     },
     {
       path: "/favoritos",
