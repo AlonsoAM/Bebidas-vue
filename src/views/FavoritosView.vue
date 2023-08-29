b275cb0a854b469e3334e924ad7a1c546f258529
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-08-29 10:00:33 -0500

    Mostrando los favoritos

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,6 +1,17 @@
-<script setup></script>
+<script setup>
+import { useFavoritosStore } from "../stores/favoritos";
+import Receta from "../components/Receta.vue";
+const storeFavoritos = useFavoritosStore();
+</script>
 <template>
   <h1 class="text-6xl font-extrabold">Favoritos</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
+    <Receta
+      v-for="bebida in storeFavoritos.favoritos"
+      :key="bebida.idDrink"
+      :receta="bebida"
+    />
+  </div>
 </template>
 
 <style scoped></style>
